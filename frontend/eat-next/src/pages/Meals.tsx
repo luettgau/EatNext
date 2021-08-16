2e10c390b320039947e6d8c0a301b3b451d8ea71
luettgau <matz.luettgau@gmx.de>
2021-08-16 17:05:58 +0200

    Update Meals.tsx

--- frontend/eat-next/src/pages/Meals.tsx
+++ frontend/eat-next/src/pages/Meals.tsx
@@ -1,11 +1,21 @@
 import {
+  IonCheckbox,
   IonContent,
   IonFab,
   IonFabButton,
   IonHeader,
   IonIcon,
+  IonInput,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
   IonPage,
+  IonRadio,
   IonTitle,
+  IonToggle,
   IonToolbar,
 } from '@ionic/react';
 import { add, person } from 'ionicons/icons';
@@ -26,12 +36,44 @@ const Meals: React.FC = () => {
             <IonIcon icon={add} />
           </IonFabButton>
         </IonFab>
-        <IonHeader collapse='condense'>
-          <IonToolbar>
-            <IonTitle size='large'>Meals</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name='Meals' />
+        <IonList>
+          <IonItemSliding>
+            <IonItem>
+              <IonLabel>Item</IonLabel>
+            </IonItem>
+            <IonItemOptions side='end'>
+              <IonItemOption onClick={() => {}}>Unread</IonItemOption>
+            </IonItemOptions>
+          </IonItemSliding>
+
+          <IonItemSliding>
+            <IonItem>
+              <IonLabel>Item</IonLabel>
+            </IonItem>
+            <IonItemOptions side='end'>
+              <IonItemOption onClick={() => {}}>Unread</IonItemOption>
+            </IonItemOptions>
+          </IonItemSliding>
+        </IonList>
+
+        <IonList>
+          <IonItem>
+            <IonLabel>Input:</IonLabel>
+            <IonInput placeholder='Your Input'></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Toggle</IonLabel>
+            <IonToggle slot='end'></IonToggle>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Radio</IonLabel>
+            <IonRadio slot='end'></IonRadio>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Checkbox</IonLabel>
+            <IonCheckbox slot='start' />
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
